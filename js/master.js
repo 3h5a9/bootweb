a7ea84da3c5240a7cd36b91ea4b13b48fc85c913
ehsanatwork <ehsanatwork@gmail.com>
2016-06-12 16:55:30 +0600

    update

--- js/master.js
+++ js/master.js
@@ -1,3 +1,49 @@
+window.onload = function() {
+      initialize();
+      }
+       
+      function initialize() {
+      var myLatlng = new google.maps.LatLng(24.368609, 88.590102);
+       
+      var myOptions = {
+      zoom: 15,
+      scrollwheel:  false,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: [{"featureType":"water","elementType":"all","stylers":[{"color":"#3b5998"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"all","elementType":"all","stylers":[{"hue":"#3b5998"},{"saturation":-22}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"on"},{"color":"#f7f7f7"},{"saturation":10},{"lightness":76}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"color":"#f7f7f7"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"color":"#8b9dc3"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"visibility":"simplified"},{"color":"#3b5998"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"on"},{"color":"#8b9dc3"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"},{"color":"#8b9dc3"}]},{"featureType":"transit.line","elementType":"all","stylers":[{"invert_lightness":false},{"color":"#ffffff"},{"weight":0.43}]},{"featureType":"road.highway","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#8b9dc3"}]},{"featureType":"administrative","elementType":"labels.icon","stylers":[{"visibility":"on"},{"color":"#3b5998"}]}]
+      }
+       
+      var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+       
+      var infowindow = new google.maps.InfoWindow({
+      content: ""
+      });
+      var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">OnePixMedia</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>H.N:26</b>, ' +
+      '<b>EhsanManzil</b> '+
+      '<b>SheikhPara, Boalia, G.P.O:6000, Rajshahi</b>'+
+      '</div>'+
+      '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+       
+      var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: "OnePixMedia"
+      });
+       
+      google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+      });
+}
+
 // Typed Js
   $(function(){
     $(".element").typed({
@@ -34,3 +80,15 @@
       resetCallback: function() {}
     });
   });
+
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
+});
